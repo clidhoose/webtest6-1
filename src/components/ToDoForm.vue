8b40e7c5e56e5a266ead9f4fd542ec4574bf66e3
clidhoose <asd1041822926@163.com>
2024-11-29 16:57:20 +0800

    Initial

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <h2 id="list-summary">{{listSummary}}</h2>
+<ul aria-labelledby="list-summary" class="stack-large">
+  <li v-for="item in ToDoItems" :key="item.id">
+    <to-do-item
+      :label="item.label"
+      :done="item.done"
+      :id="item.id"></to-do-item>
+  </li>
+</ul>
+      <input
+        type="text"
+        id="new-todo-input"
+        name="new-todo"
+        autocomplete="off"
+        v-model.lazy.trim="label"
+        class="input__lg" />
+      <button type="submit" class="btn btn__primary btn__lg">Add</button>
+    </form>
+  </template>
+  
+  
+
+<script>
+  export default {
+  methods: {
+    onSubmit() {
+  if(this.label === "") {
+    return;
+  }
+  this.$emit('todo-added', this.label);
+  this.label = "";
+}
+
+  },
+  data() {
+    return {
+      label: "",
+    };
+  },
+};
+</script>
